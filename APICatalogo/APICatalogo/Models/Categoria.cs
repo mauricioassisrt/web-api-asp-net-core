3176146308adfc0406628f8cf44b775b47557c5e
Mauricio de Assis <mauriciox8mm@hotmail.com>
2022-06-19 21:22:50 -0300

    classes criadas aula 26

---
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? ImagemUrl { get; set; }
+
+    }
+}
